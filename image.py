642bf9a581c39d166abcaea762d59e7d965a0bd5
bimarakajati <bimandugal@gmail.com>
2023-07-07 11:34:46 +0700

    First commit

---
+++ image.py
@@ -0,0 +1,30 @@
+import requests
+
+# Set the API endpoint URL
+api_endpoint = 'http://localhost:5000/detection'
+
+# Load the image file
+image_file = 'test/gabung.jpg'
+
+try:
+    # Create a dictionary with the image file
+    file = {'image': open(image_file, 'rb')}
+
+    # Send the POST request to the API endpoint
+    response = requests.post(api_endpoint, files=file)
+
+    # Check if the response was successful (status code 200)
+    if response.status_code == 200:
+        # Retrieve the JSON response
+        json_response = response.json()
+
+        # Process the response as needed
+        # ...
+
+        # Print the response
+        print(json_response)
+    else:
+        print('Request failed with status code:', response.status_code)
+
+except Exception as e:
+    print('An error occurred:', str(e))
---
+++ main.py
@@ -0,0 +1,57 @@
+import argparse, datetime, io, torch
+from PIL import Image
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S-%f"
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if not file:
+            return
+
+        img_bytes = file.read()
+        img = Image.open(io.BytesIO(img_bytes))
+        results = model([img])
+
+        results.render()  # updates results.imgs with boxes and labels
+        now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
+        img_savename = f"static/{now_time}.png"
+        Image.fromarray(results.ims[0]).save(img_savename)
+        return redirect(img_savename)
+
+    return render_template("index.html")
+
+@app.route('/detection', methods=["POST"])
+def predict():
+    if not request.method == "POST":
+        return
+
+    if request.files.get("image"):
+        image_file = request.files["image"]
+        image_bytes = image_file.read()
+        img = Image.open(io.BytesIO(image_bytes))
+        results = model(img, size=640) # reduce size=320 for faster inference
+
+        results.render()  # updates results.imgs with boxes and labels
+        now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
+        img_savename = f"static/{now_time}.png"
+        Image.fromarray(results.ims[0]).save(img_savename)
+        gambar = {'image': img_savename}
+
+        hasil = results.pandas().xyxy[0].to_dict(orient='records')
+        hasil.insert(0, gambar)
+        return hasil
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+
+    model = torch.hub.load('yolov5', 'custom', path='model/model.pt', source='local')
+    app.run(port=args.port, debug=True)  # debug=True causes Restarting with stat
